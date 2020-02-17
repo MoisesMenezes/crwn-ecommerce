95378f3ad7d52bfdd7efbeed55d288ce9521ba3a
Moisés da Silva Menezes <mmenezes@daitan.com>
2020-02-17 09:52:31 -0300

    feat: create menu-item Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import HomePage from "./components/HomePage";
+import HomePage from "./containers/HomePage";
+import "./App.css";
 
 function App() {
 	return <HomePage />;
---
+++ src/containers/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Directory from "../../components/Directory";
+
+import "./styles.scss";
+
+export default function HomePage() {
+	return (
+		<div className="homepage">
+			<Directory />
+		</div>
+	);
+}
